2e0ac60b6f1757e0b638b3aa8f7eda4eb034175f
DanielSon105 <daniel.g.barrido@gmail.com>
2015-09-30 13:57:10 -0500

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Daniel Barrido on 9/30/15.
+//  Copyright © 2015 DanielBarrido. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
